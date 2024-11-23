7014b1c47b79bc4faa1c4dc7257d27918638bd48
sidddev15 <siddharthpersonal1506@gmail.com>
2024-11-23 18:47:26 +0530

    removing readme and updating logic for temperature converter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  imports: [CommonModule, FormsModule],
 })
 export class AppComponent {
   title = 'temperature-converter';
+  inputTemperature: number | null = null; 
+  selectedUnit: string = '°C'; 
+  convertedTemperature: string = '0°C'; 
+
+  convertTemperature() {
+    if (this.inputTemperature === null) {
+      this.convertedTemperature = '∅';
+      return;
+    }
+
+    let convertedValue: number;
+
+    switch (this.selectedUnit) {
+      case '°C': // Convert to Celsius
+        convertedValue = this.inputTemperature;
+        this.convertedTemperature = `${convertedValue.toFixed(1)}°C`;
+        break;
+
+      case '°F': // Convert to Fahrenheit
+        convertedValue = (this.inputTemperature * 9) / 5 + 32;
+        this.convertedTemperature = `${convertedValue.toFixed(1)}°F`;
+        break;
+
+      case '°K': // Convert to Kelvin
+        convertedValue = this.inputTemperature + 273.15;
+        this.convertedTemperature = `${convertedValue.toFixed(1)}°K`;
+        break;
+
+      default:
+        this.convertedTemperature = 'Invallid Input';
+    }
+  }
 }
